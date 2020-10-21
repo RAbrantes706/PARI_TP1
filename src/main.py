e86453cc016aa50dd8403c061710b73dd5e749a9
Ricardo Abrantes <ricardo.abrantes@ua.pt>
2020-10-21 13:07:26 +0100

    Created the main file.
    Added a gitignore file for pycharm directories.
    Made main.py executable, with shebang.

---
+++ src/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#
+# -----------------------------------------------
+# Typing test main script.
+#
+# PARI - TP1 - GRUPO 3, October 2020
+# -----------------------------------------------
+
+def main():
+    #write some code
+    pass
+
+if __name__ == 'main':
+    main()
