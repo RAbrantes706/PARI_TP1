969527ccf41f33e5accbe7cf86c82636d2e75fff
luccamatuck <luccamatuck@ua.pt>
2020-10-23 17:34:34 +0100

    Testing Github in main.py

--- src/main.py
+++ src/main.py
@@ -5,13 +5,32 @@
 #
 # PARI - TP1 - GRUPO 3, October 2020
 # -----------------------------------------------
+import argparse
+
+import readchar
 
 from game import Query
 
+Query_List = []
+
 def main():
-    for i in range(0,10):
+
+    parser = argparse.ArgumentParser(description='Test of Texting Skills')
+    parser.add_argument('-mt', '--max_tries', type=int, default=20,
+                        help='Max Number to test')
+    args = vars(parser.parse_args())
+    print(args)
+
+    maximum_tries = args['max_tries']
+    print('Get Prepared, the chalange is about to beggin, you will type ' + str(maximum_tries) + ' chars in a range of "a" to "z" ')
+    print('press any key to continue')
+    readchar.readchar()
+
+    for i in range(0,maximum_tries):
         q = Query()
         q.ask()
+        Query_List.append(q)
+
 
 if __name__ == '__main__':
     main()
