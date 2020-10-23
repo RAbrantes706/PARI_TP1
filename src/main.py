5c5896ef9d0150da76cb209505e99c1d979bd73f
luccamatuck <luccamatuck@ua.pt>
2020-10-23 18:29:23 +0100

    Update main.py: - Add -umt store_true parse - Add conditions to run both games (timed and untimed) -

--- src/main.py
+++ src/main.py
@@ -14,15 +14,22 @@ from game import Query, timedGame, untimedGame
 def main():
 
     parser = argparse.ArgumentParser(description='Test of Texting Skills')
-    parser.add_argument('-mt', '--max_tries', type=int, default=20,
-                        help='Max Number to test')
+    parser.add_argument('-mv', '--max_value', type=int, default=20,
+                        help='Max Number of tries you want to play')
+    parser.add_argument ('-umt', '--timed_game', action='store_true',
+                         help = 'timed game mode')
     args = vars(parser.parse_args())
     print(args)
 
-    maximum_tries = args['max_tries']
+    maximum_tries = args['max_value']
+    timed_game = args['timed_game']
+
     print('Get Prepared, the chalange is about to beggin...')
 
-    Query_List = untimedGame(maximum_tries)
+    if timed_game == False:
+        Query_List = untimedGame(maximum_tries)
+    else:
+        Query_List = timedGame(maximum_tries)
 
     for query in Query_List:
         print(query.request + ' ' + query.response)
