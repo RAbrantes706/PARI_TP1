9e96062f67c29612394842798b46d85381833f11
Ricardo Abrantes <ricardo.abrantes@ua.pt>
2020-10-24 20:02:45 +0100

    Added python docs to game.py.
    Fixed some typos.

--- src/game.py
+++ src/game.py
@@ -1,25 +1,37 @@
 import random
 import readchar
-from time import time, localtime
+from time import time
 from colorama import Fore
 
-class Query: #Create a Class responsable for the logic of the game
-    def __init__(self):
-        self.request = chr(random.randint(ord('a'), ord('z'))) #self.request is a random char in a range ok "a" to "z"
 
-    def ask(self): #Ask is responsable to store information about the question, aswer and time spended
+class Query:
+    """
+    Class that holds information of one attempt on the game.
+    Upon construction a random character is generated to request the user.
+    """
+    def __init__(self):
+        self.request = chr(random.randint(ord('a'), ord('z')))  # self.request is a random char in a range ok "a" to "z"
+        self.response = chr(0)
+        self.time = 0
+        self.correct = False
+
+    def ask(self):
+        """
+        Ask the user to input the requested character.
+        :return: 0 if the user input was normal, -1 if the user wants to give up.
+        """
         print('Type letter: ' + Fore.BLUE + self.request + Fore.RESET)
 
         start = time()
         self.response = readchar.readchar()
-        self.time = time() - start #Time spended to aswer the question
+        self.time = time() - start  # Time spent to answer the question
 
         print('You typed letter:'),
-        if self.response.lower() == self.request: #If the user get it right
+        if self.response.lower() == self.request:  # If the user get it right
             self.correct = True
             print(Fore.GREEN + self.response.lower() + Fore.RESET)
         else:
-            self.correct = False #If the user get it wrong
+            self.correct = False  # If the user get it wrong
             print(Fore.RED + self.response.lower() + Fore.RESET)
 
         if self.response == ' ':
@@ -27,44 +39,55 @@ class Query: #Create a Class responsable for the logic of the game
         else:
             return 0
 
-    def __str__(self): #Define how to print the class when calls str(Query)
-        return 'I asked ' + self.request + ' and you input '\
-               + self.response + ' in ' + self.time + ' seconds'
+    def __str__(self):  # Define how to print the class when calls str(Query)
+        return 'I asked ' + self.request + ' and you input ' + self.response + ' in ' + str(self.time) + ' seconds'
 
-def timedGame(maxTime): #Creating a function to run the logic of Query.ask in a while loop for timed game
+
+def timedGame(maxTime):  # Creating a function to run the logic of Query.ask in a while loop for timed game
+    """
+    Execute a game that runs during a given time.
+    :param maxTime: Time that the user has to play the game.
+    :return: Tuple that contains (list of queries, start time, end time)
+    """
     print('Running test up to ' + str(maxTime) + ' seconds.')
-    print('Press any key to start...') #Print instructions and settings for the user
-    readchar.readchar() #Hold until the user type any key
-    startDate = time() #setting a relative time comparision
+    print('Press any key to start...')  # Print instructions and settings for the user
+    readchar.readchar()  # Hold until the user type any key
 
-    queryList = [] #Create store variable to hold information about the game (Class Query)
-    givenUp = False #Create a boolean to set user desistent
+    queryList = []  # Create list to hold information about the game (Class Query)
+    givenUp = False  # Create a variable to check if the user gives up
 
-    start = time() #setting a relative time comparision
-    while time() - start < maxTime and not givenUp: #While the max time is not reached and the user have not give up
-        query = Query() #Create a Query Class to suport the function
-        givenUp = query.ask() != 0 #Run the question and set if the user gived up
-        if time() - start < maxTime: #if time is not expired
-            queryList.append(query) #append the answer to the list
-        else: #if time is expired
-            print('The last input was out of time!') #Show the last typing was out of time
+    startDate = time()  # Save the time of the start
+    while time() - startDate < maxTime and not givenUp:  # While the max time is not reached and the user have not
+                                                         # give up
+        query = Query()  # Create a Query Class to support the function
+        givenUp = query.ask() != 0  # Run the question and check if the user has given up
+        if time() - startDate < maxTime:  # if time is not expired
+            queryList.append(query)  # append the answer to the list
+        else:  # if time has expired
+            print('The last input was out of time!')  # Show that the last typing was out of time
 
-    endDate = time() #Close the time referency
-    return (queryList, startDate, endDate) #Return data of each question, the start date and end date to main function
+    endDate = time()  # Close the time reference
+    return (queryList, startDate, endDate)  # Return data of each question, the start date and end date to main function
 
-def untimedGame(maxAttempts): #Creating a function to run the logic of Query.ask in a for loop for untimed game
+
+def untimedGame(maxAttempts):  # Creating a function to run the logic of Query.ask in a for loop for untimed game
+    """
+    Execute a game that runs during a given time.
+    :param maxAttempts: Number of attempts that the user has during the game.
+    :return: Tuple that contains (list of queries, start time, end time).
+    """
     print('Running test up to ' + str(maxAttempts) + ' attempts.')
-    print('Press any key to start...') #Print instructions and settings for the user
-    readchar.readchar() #Hold until the user type any key
-    startDate = time() #setting a relative time comparision
+    print('Press any key to start...')  # Print instructions and settings for the user
+    readchar.readchar()  # Hold until the user type any key
+    startDate = time()  # setting a relative time comparison
 
-    queryList = [] #Create store variable to hold information about the game (Class Query)
-    for i in range(0, maxAttempts): #Run the Query.ask in a for loop
+    queryList = []  # Create store variable to hold information about the game (Class Query)
+    for i in range(0, maxAttempts):  # Run the Query.ask in a for loop
         query = Query()
         givenUp = query.ask() != 0
-        if(givenUp):
+        if (givenUp):
             break
         queryList.append(query)
 
-    endDate = time() #Close the time referency
-    return (queryList, startDate, endDate) #Return data of each question, the start date and end date to main function
+    endDate = time()  # Close the time reference
+    return (queryList, startDate, endDate)  # Return data of each question, the start date and end date to main function
--- src/main.py
+++ src/main.py
@@ -6,78 +6,90 @@
 # PARI - TP1 - GRUPO 3, October 2020
 # -----------------------------------------------
 
-
 import argparse
 from colorama import Fore
-from game import timedGame, untimedGame #Import functions from "game.py"
+from game import timedGame, untimedGame  # Import functions from "game.py"
 from collections import namedtuple
 from time import ctime
 from pprint import pprint
 
-Input = namedtuple('Input', ['requested', 'received', 'duration']) #Create a Tuple wich will recieve all data of each try in the game
+# Create a Tuple which will receive all data of each try in the game
+Input = namedtuple('Input', ['requested', 'received', 'duration'])
+
 
 def main():
+    # Create a parser to the script
+    parser = argparse.ArgumentParser(description=Fore.YELLOW + 'Test of Texting Skills. I challenge YOU, to type the ma'
+                                     'ximum of correct chars you can.' + Fore.GREEN)
 
-    parser = argparse.ArgumentParser(description=Fore.YELLOW + 'Test of Texting Skills. I chalange YOU, to type the maximum of correct chars you can.' + Fore.GREEN) #Create a Parse to the script
+    parser.add_argument('-umt', '--timed_game', action='store_true',
+                        help=Fore.RED + 'Activate Timed game mode' + Fore.YELLOW + ', if you select it, the Max Number'
+                        ' you tipped will become the max duration of the challenge' + Fore.GREEN)
 
-    parser.add_argument ('-umt', '--timed_game', action='store_true',
-                         help = Fore.RED + 'Activate Timed game mode' + Fore.YELLOW + ', if you select it, the Max Number you tipped will become the max duration of the challenge' + Fore.GREEN)
+    parser.add_argument('-mv', '--max_value', type=int, default=20,
+                        help=Fore.YELLOW + 'Max Number of tries you want in ' + Fore.RED + 'untimed game mode'
+                             + Fore.BLUE + ' OR' + Fore.YELLOW + ' Max seconds you want to play in' + Fore.RED + ' tim'
+                             'ed game mode' + Fore.RESET + "\n" + Fore.CYAN + '\n Default = 20 ' + Fore.RESET)
 
-    parser.add_argument('-mv', '--max_value', type=int, default=20,  #Adding Parses
-                        help= Fore.YELLOW + 'Max Number of tries you want in ' + Fore.RED + 'untimed game mode'
-                        + Fore.BLUE + ' OR' + Fore.YELLOW + ' Max seconds you want to play in' + Fore.RED + ' untimed game mode'+ Fore.RESET +
-                        "\n" + Fore.CYAN + '\n Default = 20 ' + Fore.RESET)
+    args = vars(parser.parse_args())  # Create a Variable to keep information of parser input
+    print(args)  # Show to the user the type of game with the number of tries or seconds of duration
 
-    args = vars(parser.parse_args()) #Create a Variable to keep information of parser input
-    print(args) #Show to the user the type of game with the number of tries or seconds of duration
+    # Create an integer Variable to hold information about the max value inputted
+    maximum_tries = args['max_value']
+    # Create a boolean Variable to set into "timed" and "untimed" game modes.
+    timed_game = args['timed_game']
 
-    maximum_tries = args['max_value'] #Create an integer Variable to hold information about the max value inputed
-    timed_game = args['timed_game'] #Create a boolean Variable to set into "timed" and "untimed" game modes.
+    # Print a challenging message to scary the user
+    print('Get Prepared, the challenge is about to begin... (You will fail)')
 
-    print('Get Prepared, the chalange is about to beggin... (You will fail)') #Print a challenging message to scary the user
-
-    if not timed_game: #This "if" function must indicate which game mode should be runned
-        Query_List = untimedGame(maximum_tries) #Select untimed mode type and set the maximun duration for "maximun_tries" second
+    # This "if" function must indicate which game mode should be ran
+    if not timed_game:
+        # Select timed mode type and set the maximum time to "maximum_tries"
+        query_list = untimedGame(maximum_tries)
     else:
-        Query_List = timedGame(maximum_tries) #Select timed mode type and set the maximun tries for "maximun_tries"
+        # Select untimed mode type and set the maximum duration for "maximum_tries" second
+        query_list = timedGame(maximum_tries)
 
-    hits = 0  #Setting global control variables
+    # Starting to process the information of the user challenge (He failed, of course)
+    hits = 0
     hit_total_duration = 0
     miss_total_duration = 0
     inputs = []
-    for query in Query_List[0]: #Stock every data about each try of the user
+    for query in query_list[0]:  # Stock every data about each try of the user
         inputs.append(Input(query.request, query.response, query.time))
-        if query.correct: #Check if the user was sucessfull and count the time spended
+        if query.correct:  # Check if the user was successful and count the time spent
             hits += 1
-            hit_total_duration += query.time  #If he typed it right
+            hit_total_duration += query.time  # If he typed it right
         else:
-            miss_total_duration += query.time #Or wrong
+            miss_total_duration += query.time  # Or wrong
 
-    #Starting to process the information getted in the user chalange (He failed, of course)
-    accuracy = 0 #Creating Global Variables
+    # Calculate some statistical data of the game
+    accuracy = 0  # Creating Global Variables
     type_average_duration = 0
     type_miss_average_duration = 0
     type_hit_average_duration = 0
-    if not hits == len(Query_List[0]): #Avoiding future erros
-        type_miss_average_duration = miss_total_duration/(len(Query_List[0])-hits)
-    if not hits == 0: #Avoiding future erros
-        type_hit_average_duration = hit_total_duration/hits
-    if not len(Query_List[0]) == 0: #Avoiding future erros
-        accuracy = hits*1.0/len(Query_List[0])
-        type_average_duration = (Query_List[2] - Query_List[1]) / len(Query_List[0])
+    if not hits == len(query_list[0]):  # Avoiding future errors
+        type_miss_average_duration = miss_total_duration / (len(query_list[0]) - hits)
+    if not hits == 0:  # Avoiding future errors
+        type_hit_average_duration = hit_total_duration / hits
+    if not len(query_list[0]) == 0:  # Avoiding future errors
+        accuracy = hits * 1.0 / len(query_list[0])
+        type_average_duration = (query_list[2] - query_list[1]) / len(query_list[0])
 
-    stats = {'inputs':inputs, #Create a Dictionary with all information processed of the Challange
-             'test_duration':Query_List[2]-Query_List[1],
-             'test_start':ctime(Query_List[1]),
-             'test_end':ctime(Query_List[2]),
-             'number_of_types':len(Query_List[0]),
-             'number_of_hits':hits,
-             'accuracy':accuracy,
-             'type_average_duration':type_average_duration,
-             'type_hit_average_duration':type_hit_average_duration,
-             'type_miss_average_duration':type_miss_average_duration}
+    # Create a Dictionary with all information processed of the Challenge
+    stats = {'inputs': inputs,
+             'test_duration': query_list[2] - query_list[1],
+             'test_start': ctime(query_list[1]),
+             'test_end': ctime(query_list[2]),
+             'number_of_types': len(query_list[0]),
+             'number_of_hits': hits,
+             'accuracy': accuracy,
+             'type_average_duration': type_average_duration,
+             'type_hit_average_duration': type_hit_average_duration,
+             'type_miss_average_duration': type_miss_average_duration}
 
-    pprint(stats) #Pretty Print it !!
+    pprint(stats)  # Pretty Print it !!
+
 
 if __name__ == '__main__':
     main()
